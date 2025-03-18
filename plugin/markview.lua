351366e7d2794456465dc6c48342177114ef15c0
Shawon <mdmoinulhossainshawon@gmail.com>
2025-03-18 16:33:24 +0600

    fix: Markview no longer overwrites user source list for blink.cmp
    Ref: #317

--- plugin/markview.lua
+++ plugin/markview.lua
@@ -69,13 +69,13 @@ if vim.g.markview_blink_loaded == false and blink ~= nil then
 
 			if vim.islist(default) then
 				config.sources.per_filetype[ft] = vim.list_extend(default, { "markview" });
-			else
-				--- Empty context.
-				local can_exec, exec = pcall(default, {});
-
-				if can_exec and vim.islist(exec) then
-					config.sources.per_filetype[ft] = vim.list_extend(exec, { "markview" });
-				end
+			-- else
+			-- 	--- Empty context.
+			-- 	local can_exec, exec = pcall(default, {});
+			--
+			-- 	if can_exec and vim.islist(exec) then
+			-- 		config.sources.per_filetype[ft] = vim.list_extend(exec, { "markview" });
+			-- 	end
 			end
 		else
 			pcall(blink.add_filetype_source, ft, "markview");
