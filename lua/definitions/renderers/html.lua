473a2cb0354f3c5bd5e6cb94876b3cda3b2f2734
Shawon <mdmouinulhossainshawon@gmail.com>
2025-06-06 09:44:33 +0600

    doc: Finalized type defs for html

--- lua/definitions/renderers/html.lua
+++ lua/definitions/renderers/html.lua
@@ -27,29 +27,10 @@
 ---
 ---@field closing_tag_offset? fun(range: integer[]): integer[] Modifies the closing </tag>'s range.
 ---@field node_offset? fun(range: integer[]): integer[] Modifies the element's range.
----@field on_closing_tag? config.extmark | fun(tag: table): config.extmark Extmark configuration to use on the closing </tag>.
----@field on_node? config.extmark Extmark configuration to use on the element.
----@field on_opening_tag? config.extmark Extmark configuration to use on the opening <tag>.
+---@field on_closing_tag? table | fun(tag: table): table Extmark configuration to use on the closing </tag>.
+---@field on_node? table Extmark configuration to use on the element.
+---@field on_opening_tag? table Extmark configuration to use on the opening <tag>.
 ---@field opening_tag_offset? fun(range: integer[]): integer[] Modifies the opening <tag>'s range.
-
---- Parsed version of an HTML container element.
----@class markview.parsed.html.container_elements
----
----@field class "html_container_element"
----
----@field opening_tag __container.data Table containing information regarding the opening tag.
----@field closing_tag __container.data Table containing information regarding the closing tag.
----
----@field name string Tag name(in lowercase).
----
----@field text string[] Text of this node.
----@field range node.range Range of this node.
-
---- Container element segment data.
----@class __container.data
----
----@field text string Text inside this segment.
----@field range integer[] Range of this segment(Result of `{ TSNode:range() }`).
 
 ------------------------------------------------------------------------------
 
@@ -57,14 +38,7 @@
 ---@class markview.config.html.headings
 ---
 ---@field enable boolean
----@field [string] config.extmark Configuration for <h[n]></h[n]>.
-
----@class __html.headings
----
----@field class "html_heading"
----@field level integer Heading level.
----@field range node.range
----@field text string[]
+---@field [string] table Configuration for <h[n]></h[n]>.
 
 ------------------------------------------------------------------------------
 
@@ -78,15 +52,5 @@
 ---@class markview.config.html.void_elements.opts
 ---
 ---@field node_offset? fun(range: integer[]): table
----@field on_node config.extmark | fun(tag: markview.config.html.void_elements): config.extmark
-
---- Parsed version of a void element.
----@class markview.config.html.void_elements
----
----@field class "html_void_element"
----
----@field name string Element name(always in lowercase).
----
----@field text string[]
----@field range node.range
+---@field on_node table | fun(tag: markview.config.html.void_elements): table
 
