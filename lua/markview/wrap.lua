ef262ef0e0b7e48d724f1b92bcc044d3a0f07bed
itsjunetime <junewelker@gmail.com>
2025-09-23 10:47:50 -0500

    ok it does what i think now

--- lua/markview/wrap.lua
+++ lua/markview/wrap.lua
@@ -31,7 +31,6 @@ end
 ---@param opts markview.wrap.opts
 wrap.wrap_indent = function (buffer, opts)
 	---|fS
-	-- vim.notify("CALLING WRAP_INDENT WITH ROW " .. tostring(opts.row) .. " whose contents are '" .. (opts.line or "") .. "'");
 
 	local win = utils.buf_getwin(buffer);
 
@@ -40,8 +39,6 @@ wrap.wrap_indent = function (buffer, opts)
 		full_indent_text = full_indent_text .. txt_and_hi[1];
 	end
 	local indent_len = vim.fn.strdisplaywidth(full_indent_text);
-
-	-- vim.notify("full indent text is '" .. full_indent_text .. "', and its len is " .. tostring(indent_len));
 
 	local win_width = vim.api.nvim_win_get_width(win);
 	local textoff = vim.fn.getwininfo(win)[1].textoff;
@@ -57,13 +54,9 @@ wrap.wrap_indent = function (buffer, opts)
 	local start_idx_off = 1;
 	local num_indents = math.floor(dsp_w / W);
 
-	local start_disp_row = vim.fn.screenpos(win, opts.row + 1, 0).row;
-
-	-- vim.notify("we think that there are " .. num_indents .. " indents for W of " .. W .. " and dsp_w of " .. tostring(dsp_w) .. "(win_width is " .. tostring(win_width) .. ", textoff is " .. tostring(textoff) .. ")");
+	local start_disp_row = vim.fn.screenpos(win, opts.row + 1, 1).row;
 
 	for line_to_indent = 0, num_indents do
-		-- vim.notify("ok we're looking at line " .. line_to_indent .. " for opts.row " .. opts.row);
-
 		if start_idx_off > 1 then
 			start_idx_off = start_idx_off + win_width;
 		end
@@ -73,11 +66,15 @@ wrap.wrap_indent = function (buffer, opts)
 		while true do
 			local pos = vim.fn.screenpos(win, opts.row + 1, start_idx_off);
 
-			-- vim.notify("got a pos at col " .. pos.col .. ", row " .. pos.row .. " for start_idx_off " .. start_idx_off .. ", offset " .. offset);
-
 			local x = pos.col - offset;
 
 			if not passed_start then
+				-- if we get here, then we've ran offscreen, like not the whole line can fit or something.
+				-- Just bail.
+				if pos.row == 0 then
+					return;
+				end
+
 				-- if the line we started on + the number of lines we should have traversed by now
 				-- is a row number greater than the row that this character appears on, we need to
 				-- go to the next character 'cause we're probably right by the beginning of the row,
@@ -89,14 +86,11 @@ wrap.wrap_indent = function (buffer, opts)
 
 				-- if we are on a character *past* the first one, but we haven't already marked *passed_start*,
 				-- then we gotta jump back by a bit more than we think
-				if (x > 1) and not passed_start then
-					start_idx_off = (start_idx_off - (pos.col + 1)) + textoff;
+				if x > 1 then
+					start_idx_off = start_idx_off - (pos.col + 1);
 					goto continue;
 				end
 			end
-
-			-- vim.notify("got past fail loop with col " .. pos.col .. ", row " .. pos.row .. " for start_idx_off " .. start_idx_off);
-			-- vim.notify("x is " .. tostring(x) .. ", passed_start? " .. tostring(passed_start));
 
 			if x ~= 1 then
 				goto next_line;
@@ -125,8 +119,6 @@ wrap.wrap_indent = function (buffer, opts)
 				extmark_opts.id = extmark[1];
 				extmark_opts.virt_text = vim.list_extend(extmark_opts.virt_text, extmark[4].virt_text);
 			end
-
-			-- vim.notify("setting extmark...");
 
 			vim.api.nvim_buf_set_extmark(buffer, opts.ns, opts.row, start_idx_off - 1, extmark_opts);
 			start_idx_off = start_idx_off + 1;
