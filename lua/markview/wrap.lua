01825b0e3c1ed5fb40fcaf0d9ada42b0a8404f58
Shawon <mdmoinulhossainshawon@gmail.com>
2025-09-27 18:31:51 +0600

    refacto(wrap)!: Removed relaince on `screenpos` for wrap support
    Based on #393.
    
    Co-authored-by: itsjunetime <junewelker@gmail.com>

--- lua/markview/wrap.lua
+++ lua/markview/wrap.lua
@@ -69,6 +69,77 @@ wrap.wrap_indent = function (buffer, range, indent)
 	---|fE
 end
 
+--[[
+Wraps text based on *heuristics*. Relies on `virt_text_win_col`.
+Based on the original pull request(#393) by @itsjunetime.
+
+NOTE: This works by positioning the `extmark` in the estimated wrap position.
+
+Then we use `virt_text_win_col` to place it at the start of that *wrapped* line.
+]]
+---@param buffer integer
+---@param win integer
+---@param row integer
+---@param ns integer
+---@param indent [ string, string? ][]
+wrap.huristic_wrap = function (buffer, win, row, ns, indent)
+	---|fS
+
+	local textoff = vim.fn.getwininfo(win)[1].textoff;
+
+	local win_w = vim.api.nvim_win_get_width(win);
+	local width = win_w - textoff;
+
+	local text = vim.api.nvim_buf_get_lines(buffer, row, row + 1, false)[1] or "";
+
+	---|fS "chunk: Calculate text width"
+
+	---@type table<integer, integer> Maps a `display width` to it's `character index`.
+	local dsp_char_map = {};
+	local text_width = 0;
+
+	for c, char in ipairs(vim.fn.split(text, "\\zs")) do
+		local w = vim.fn.strdisplaywidth(char);
+
+		for _w = 1, w, 1 do
+			dsp_char_map[text_width + _w] = c;
+		end
+
+		text_width = text_width + w;
+	end
+
+	---|fE
+
+	---@type integer Number of soft-wraps.
+	local divisions = math.floor(text_width / width);
+
+	for d = 1, divisions, 1 do
+		local char = dsp_char_map[d * width];
+
+		local indent_opts = {
+			undo_restore = false, invalidate = true,
+			right_gravity = false,
+
+			virt_text_pos = "inline",
+			hl_mode = "combine",
+
+			virt_text = indent;
+			virt_text_win_col = 0
+		};
+
+
+		vim.api.nvim_buf_set_extmark(
+			buffer,
+			ns,
+			row,
+			char - 1,
+			indent_opts
+		);
+	end
+
+	---|fE
+end
+
 --[[ Provides `wrapped indentation` to some text. ]]
 ---@param buffer integer
 wrap.render = function (buffer, ns)
@@ -82,47 +153,7 @@ wrap.render = function (buffer, ns)
 	end
 
 	local function render_line (row, indent)
-		local textoff = vim.fn.getwininfo(win)[1].textoff;
-
-		local win_x = vim.api.nvim_win_get_position(win)[2];
-
-		local text = vim.api.nvim_buf_get_lines(buffer, row, row + 1, false)[1];
-		local chars = vim.fn.split(text, "\\zs");
-
-		local c = 0;
-		local before_start = true;
-
-		local last_screencol = math.huge;
-
-		for _, char in ipairs(chars) do
-			c = c + #char;
-
-			local x = vim.fn.screenpos(win, row + 1, c).col - (win_x + textoff);
-
-			if x < last_screencol and before_start == true then
-				before_start = false;
-			elseif  x < last_screencol then
-				local indent_opts = {
-					undo_restore = false, invalidate = true,
-					right_gravity = false,
-
-					virt_text_pos = "inline",
-					hl_mode = "combine",
-
-					virt_text = indent;
-				};
-
-				vim.api.nvim_buf_set_extmark(
-					buffer,
-					ns,
-					row,
-					c - 1,
-					indent_opts
-				);
-			end
-
-			last_screencol = x;
-		end
+		wrap.huristic_wrap(buffer, win, row, ns, indent);
 	end
 
 	for row, indent in pairs(wrap.cache[buffer] or {}) do
