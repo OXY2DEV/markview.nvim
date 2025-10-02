22c1c8ef1422b00360273722ad305a747bd4369b
Shawon <mdmoinulhossainshawon@gmail.com>
2025-10-02 08:58:28 +0600

    feat(config, html): Added support for `<kbd></kbd>`
    Closes #402

--- lua/markview/config/html.lua
+++ lua/markview/config/html.lua
@@ -29,6 +29,101 @@ return {
 			on_node = { hl_group = "Italic" },
 			on_closing_tag = { conceal = "" },
 		},
+		["^kbd$"] = function (_, item)
+			local hl = "MarkviewInlineCode";
+			local output = {
+				on_opening_tag = { conceal = "" },
+				on_node = {
+					virt_text_pos = "inline",
+					right_gravity = true,
+					hl_group = hl
+				},
+				on_closing_tag = { conceal = "", hl_mode = "combine", virt_text_pos = "inline", virt_text = { { " ", hl } } },
+			};
+
+			if not item then
+				return output;
+			end
+
+			---@type integer Start column of the node.
+			local start_col = item.range.col_start;
+
+			---@type integer[] Output of `TSNode:range()` on the opening tag.
+			local open_range = item.opening_tag.range;
+			---@type integer[] Output of `TSNode:range()` on the closing tag.
+			local close_range = item.closing_tag.range;
+
+			---@type string Normalized keymap.
+			local key = string.upper(
+				string.sub(
+					item.text[1],
+					(open_range[4] + 1) - start_col,
+					close_range[2] - start_col
+				)
+			);
+
+			if string.match(key, "^%<C") or string.match(key, "CTRL") then
+				hl = "MarkviewPalette3";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { "  ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			elseif string.match(key, "^%<S") or string.match(key, "SHIFT") then
+				hl = "MarkviewPalette4";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { " 󰘶 ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			elseif
+				string.match(key, "^%<M") or
+				string.match(key, "OPT") or string.match(key, "OPTION") or
+				string.match(key, "ALT") or string.match(key, "META")
+			then
+				hl = "MarkviewPalette2";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { " 󰘵 ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			elseif string.match(key, "CMD") or string.match(key, "COMMAND") then
+				hl = "MarkviewPalette5";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { " 󰘳 ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			elseif string.match(key, "CAPS.LOCK") then
+				hl = "MarkviewPalette1";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { " 󰘲 ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			elseif string.match(key, "SPACE") then
+				hl = "MarkviewPalette5";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { " 󱁐 ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			elseif string.match(key, "ENTER") then
+				hl = "MarkviewPalette2";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { " 󰌑 ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			elseif string.match(key, "TAB") then
+				hl = "MarkviewPalette3";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { " 󰌒 ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			else
+				hl = "MarkviewPalette6";
+
+				output.on_node.hl_group = hl;
+				output.on_node.virt_text = { { " 󰌌 ", hl } };
+				output.on_closing_tag.virt_text = { { " ", hl } };
+			end
+
+			return output;
+		end,
 		["^mark$"] = {
 			on_opening_tag = { conceal = "" },
 			on_node = { hl_group = "MarkviewPalette1" },
