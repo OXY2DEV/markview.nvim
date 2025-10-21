ed2a7e02eb35522e43d4f6abf03ab00c0cbfca35
Shawon <mdmoinulhossainshawon@gmail.com>
2025-10-21 19:56:59 +0600

    refactor: Moved more actions to the actions submodule

--- lua/markview/actions.lua
+++ lua/markview/actions.lua
@@ -104,18 +104,18 @@ end
 --- Renders preview.
 ---@param _buffer integer?
 ---@param _state? markview.state.buf
----@param config? markview.config
-actions.render = function (_buffer, _state, config)
+---@param _config? markview.config
+actions.render = function (_buffer, _state, _config)
 	---|fS
 
 	local parser = require("markview.parser");
 	local renderer = require("markview.renderer");
 	local spec = require("markview.spec");
 
-	spec.tmp_setup(config);
+	spec.tmp_setup(_config);
 
 	local buffer = _buffer or vim.api.nvim_get_current_buf();
-	local state = _state or require("markview.state").buffer_state(buffer, true);
+	local state = _state or require("markview.state").get_buffer_state(buffer, true);
 
 	---@cast state markview.state.buf
 
@@ -153,6 +153,167 @@ actions.render = function (_buffer, _state, config)
 	end
 
 	spec.tmp_reset();
+
+	---|fE
+end
+
+------------------------------------------------------------------------------
+
+--- Executes callbacks/autocmds.
+---@param autocmd string
+---@param ... any
+actions.autocmd = function (autocmd, ...)
+	---|fS
+
+	local args = { ... };
+	local map = {
+		on_attach = { "MarkviewAttach", { buffer = args[1], windows = args[2] } },
+		on_enable = { "MarkviewEnable", { buffer = args[1], windows = args[2] } },
+		on_hybrid_enable = { "MarkviewHybridEnable", { buffer = args[1], windows = args[2] } },
+		on_hybrid_disable = { "MarkviewHybridDisable", { buffer = args[1], windows = args[2] } },
+		on_disable = { "MarkviewDisable", { buffer = args[1], windows = args[2] } },
+		on_detach = { "MarkviewDetach", { buffer = args[1], windows = args[2] } },
+		on_splitview_open = { "MarkviewSplitviewOpen", { source = args[1], preview_buffer = args[2], preview_window = args[3] } },
+		on_splitview_close = { "MarkviewSplitviewClose", { source = args[1], preview_buffer = args[2], preview_window = args[3] } },
+	};
+
+	if not map[autocmd] then
+		return;
+	end
+
+	local spec = require("markview.spec");
+
+	local callbacks = spec.get({ "preview", "callbacks" }, { fallback = nil, ignore_enable = true });
+	pcall(callbacks[autocmd], ...);
+	vim.api.nvim_exec_autocmds("User", { pattern = map[autocmd][1], data = map[autocmd][2] });
+
+	-- health.notify("trace", {
+	-- 	level = 1,
+	-- 	message = {
+	-- 		{ "Callback: ", "Special" },
+	-- 		{ " " .. autocmd .. " ", "DiagnosticVirtualTextInfo" }
+	-- 	}
+	-- });
+
+	---|fE
+end
+
+actions.set_query = function (buffer)
+	---|fS
+
+	local default_path = vim.treesitter.query.get_files("markdown", "highlights")[1];
+
+	if not default_path then
+		return;
+	end
+
+	local default = table.concat(
+		vim.fn.readfile(default_path),
+		"\n"
+	);
+	vim.g.__markdown_default_hl_query = default;
+
+	local capture_1 = table.concat({
+		"%(fenced_code_block",
+		"%s*%(fenced_code_block_delimiter%) @markup.raw.block",
+		'%s*%(#set! conceal ""%)',
+		'%s*%(#set! conceal_lines ""%)%)',
+	}, "");
+
+	local capture_2 = table.concat({
+		"%(fenced_code_block",
+		'%s*%(info_string',
+		'%s*%(language%) @label',
+		'%s*%(#set! conceal ""%)',
+		'%s*%(#set! conceal_lines ""%)%)%)',
+	}, "");
+
+	local updated = string.gsub(default, capture_1, ""):gsub(capture_2, "");
+	vim.treesitter.query.set("markdown", "highlights", updated);
+
+	vim.treesitter.stop(buffer);
+	vim.treesitter.start(buffer)
+
+	--[[]
+		NOTE: Only set query for `buffer`.
+
+		By restoring the active query, we can prevent new buffers from using *our* queries.
+		This allows us to avoid issues with other plugins that make use of the default queries.
+
+		See #404
+	]]
+	vim.treesitter.query.set("markdown", "highlights", vim.g.__markdown_default_hl_query);
+
+	---|fE
+end
+
+actions.reset_query = function (buffer)
+	---|fS
+
+	if not vim.g.__markdown_default_hl_query then
+		return;
+	end
+
+	vim.treesitter.query.set("markdown", "highlights", vim.g.__markdown_default_hl_query);
+
+	vim.treesitter.stop(buffer);
+	vim.treesitter.start(buffer)
+
+	---|fE
+end
+
+actions.attach = function (buffer, _state)
+	---|fS
+
+	---@type integer
+	buffer = buffer or vim.api.nvim_get_current_buf();
+
+	local spec = require("markview.spec");
+	local state = require("markview.state");
+
+	if state.can_attach(buffer) == false then
+		return;
+	end
+
+	-- health.notify("trace", {
+	-- 	level = 8,
+	-- 	message = string.format("Attached: %d", buffer)
+	-- });
+	-- health.__child_indent_in();
+
+	state.attach(buffer, _state);
+
+	---@type boolean Whether `gx` should be remapped.
+	local map_gx = spec.get({ "preview", "map_gx" }, { ignore_enable = true, fallback = true });
+
+	if map_gx then
+		vim.api.nvim_buf_set_keymap(buffer, "n", "gx", "<CMD>Markview open<CR>", {
+			desc = "Tree-sitter based link opener from `markview.nvim`."
+		});
+	end
+
+	actions.autocmd("on_attach", buffer, vim.fn.win_findbuf(buffer))
+	local buf_state = state.get_buffer_state(buffer, false);
+
+	if buf_state and buf_state.enable == true then
+		actions.set_query(buffer);
+
+		actions.autocmd("on_enable", buffer, vim.fn.win_findbuf(buffer))
+		actions.render(buffer);
+
+		if buf_state.hybrid_mode then
+			actions.autocmd("on_hybrid_enable", buffer, vim.fn.win_findbuf(buffer))
+		else
+			actions.autocmd("on_hybrid_disable", buffer, vim.fn.win_findbuf(buffer))
+		end
+	else
+		actions.reset_query(buffer);
+		actions.clear(buffer);
+
+		actions.autocmd("on_disable", buffer, vim.fn.win_findbuf(buffer))
+	end
+
+	-- health.__child_indent_de();
 
 	---|fE
 end
--- lua/markview/state.lua
+++ lua/markview/state.lua
@@ -16,6 +16,51 @@ state.vars = {
 	modified_queries = false;
 };
 
+
+state.buf_safe = function (buffer)
+	---|fS
+
+	if type(buffer) ~= "number" then
+		return false;
+	elseif vim.api.nvim_buf_is_valid(buffer) == false then
+		return false;
+	elseif vim.v.exiting ~= vim.NIL then
+		--[[
+			BUG(no-repro): Crash when exiting Neovim.
+
+			There used to be a bug that caused crashes when exiting `Neovim`.
+			I can no longer reproduce it on `main`. So, this is *legacy code*.
+		]]
+		return false;
+	end
+
+	return true;
+
+	---|fE
+end
+
+state.win_safe = function (window)
+	---|fS
+
+	if not window then
+		return false;
+	elseif vim.api.nvim_win_is_valid(window) == false then
+		return false;
+	elseif vim.api.nvim_win_get_tabpage(window) ~= vim.api.nvim_get_current_tabpage() then
+		--[[
+			NOTE: Some window functions can't be called on some windows.
+
+			This happens if the window isn't *visible*/not in the **current tab**.
+		]]
+		return false;
+	end
+
+	return true;
+
+	---|fE
+end
+
+
 --- Enable/Disable `markview.nvim`
 ---@param to boolean
 state.enable = function (to)
@@ -25,7 +70,7 @@ end
 ---@param buffer? integer
 ---@param default? boolean
 ---@return markview.state.buf?
-state.buffer_state = function (buffer, default)
+state.get_buffer_state = function (buffer, default)
 	if type(buffer) ~= "number" then
 		return;
 	elseif state.vars.buffer_states[buffer] then
@@ -45,6 +90,46 @@ state.buffer_state = function (buffer, default)
 		enable = enable,
 		hybrid_mode = hm_enable
 	};
+end
+
+---@param buffer integer
+---@param new_state markview.state.buf
+state.set_buffer_state = function (buffer, new_state)
+	if state.vars.buffer_states[buffer] and not new_state then
+		return;
+	end
+
+	local spec = require("markview.spec");
+
+	---@type boolean Should preview be enabled on the buffer?
+	local enable = spec.get({ "preview", "enable" }, { fallback = true, ignore_enable = true });
+	---@type boolean Should hybrid mode be enabled on the buffer?
+	local hm_enable = spec.get({ "preview", "enable_hybrid_mode" }, { fallback = true, ignore_enable = true });
+
+	state.vars.buffer_states[buffer] = vim.tbl_extend("force", {
+		enable = enable,
+		hybrid_mode = hm_enable
+	}, new_state or {});
+end
+
+state.can_attach = function (buffer)
+	if state.buf_safe(buffer) == false then
+		return false;
+	elseif vim.list_contains(state.vars.attached_buffers, buffer) then
+		return false;
+	end
+
+	return true;
+end
+
+---@param buffer integer
+state.attach = function (buffer, _state)
+	if state.can_attach(buffer) == false then
+		return;
+	end
+
+	table.insert(state.vars.attached_buffers, buffer);
+	state.set_buffer_state(buffer, _state);
 end
 
 return state;
