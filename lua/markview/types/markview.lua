b3faeee9937e7d0bffb7683b1e068185ae4ba609
Shawon <mdmoinulhossainshawon@gmail.com>
2025-10-23 22:19:47 +0600

    doc: Updated help files

--- lua/markview/types/markview.lua
+++ lua/markview/types/markview.lua
@@ -26,13 +26,12 @@
 ---@class markview.config
 ---
 ---@field experimental? markview.config.experimental
----@field preview? markview.config.preview
----@field renderers? table<string, function>
----
 ---@field html? markview.config.html
 ---@field latex? markview.config.latex
 ---@field markdown? markview.config.markdown
 ---@field markdown_inline? markview.config.markdown_inline
+---@field preview? markview.config.preview
+---@field renderers? table<string, function>
 ---@field typst? markview.config.typst
 ---@field yaml? markview.config.yaml
 
