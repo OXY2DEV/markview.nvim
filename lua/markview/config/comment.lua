ab00aa4fb1270e55ecc18039a062c2ccd6d02d48
Shawon <mdmouinulhossainshawon@gmail.com>
2026-01-09 12:41:42 +0600

    feat(config, comment, tasks): Added more task type configs
    feat(config, comment): Added missing config options

--- lua/markview/config/comment.lua
+++ lua/markview/config/comment.lua
@@ -1,5 +1,19 @@
+---@type markview.config.comment
 return {
 	enable = true,
+
+	autolinks = {
+		enable = true,
+
+		default = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰋽 ",
+
+			hl = "MarkviewPalette6",
+		},
+	},
 
 	code_blocks = {
 		enable = true,
@@ -37,24 +51,15 @@ return {
 	},
 
 	inline_codes = {
-			padding_left = " ",
-			padding_right = " ",
+		padding_left = " ",
+		padding_right = " ",
 
-			hl = "MarkviewInlineCode",
-	},
-
-	tasks = {
-		default = {
-			padding_left = " ",
-			padding_right = " ",
-
-			icon = "󰋽 ",
-
-			hl = "MarkviewPalette1",
-		},
+		hl = "MarkviewInlineCode",
 	},
 
 	issues = {
+		enable = true,
+
 		default = {
 			padding_left = " ",
 			padding_right = " ",
@@ -64,7 +69,202 @@ return {
 			hl = "MarkviewPalette6",
 		},
 	},
+
 	mentions = {
+		enable = true,
+
+		default = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰋽 ",
+
+			hl = "MarkviewPalette6",
+		},
+	},
+
+	taglinks = {
+		enable = true,
+
+		default = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰋽 ",
+
+			hl = "MarkviewPalette6",
+		},
+	},
+
+	tasks = {
+		enable = true,
+
+		default = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰄰 ",
+
+			hl = "MarkviewPalette1",
+		},
+
+		["^feat"] = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󱕅 ",
+
+			hl = "MarkviewPalette7",
+		},
+
+		praise = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󱥋 ",
+
+			hl = "MarkviewPalette3",
+		},
+
+		["^suggest"] = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰣖 ",
+
+			hl = "MarkviewPalette2",
+		},
+
+		thought = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰧑 ",
+
+			hl = "MarkviewPalette0",
+		},
+
+		note = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰠮 ",
+
+			hl = "MarkviewPalette5",
+		},
+
+		["^info"] = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰋼 ",
+
+			hl = "MarkviewPalette0",
+		},
+
+		xxx = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰓽 ",
+
+			hl = "MarkviewPalette0",
+		},
+
+		["^nit"] = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰩰 ",
+
+			hl = "MarkviewPalette6",
+		},
+
+		["^warn"] = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = " ",
+
+			hl = "MarkviewPalette3",
+		},
+
+		fix = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰁨 ",
+
+			hl = "MarkviewPalette7",
+		},
+
+		hack = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󱍔 ",
+
+			hl = "MarkviewPalette1",
+		},
+
+		typo = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = " ",
+
+			hl = "MarkviewPalette0",
+		},
+
+		wip = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰦖 ",
+
+			hl = "MarkviewPalette2",
+		},
+
+		issue = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = " ",
+
+			hl = "MarkviewPalette1",
+		},
+
+		["error"] = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰅙 ",
+
+			hl = "MarkviewPalette1",
+		},
+
+		fixme = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰶯 ",
+
+			hl = "MarkviewPalette4",
+		},
+
+		deprecated = {
+			padding_left = " ",
+			padding_right = " ",
+
+			icon = "󰩆 ",
+
+			hl = "MarkviewPalette1",
+		},
+	},
+
+	urls = {
+		enable = true,
+
 		default = {
 			padding_left = " ",
 			padding_right = " ",
